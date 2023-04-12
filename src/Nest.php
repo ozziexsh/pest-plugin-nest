8da02bb9d6115b1df5f93b5aed438e8f25e423fb
Ozzie Neher <oz.neher@gmail.com>
2023-04-12 12:02:05 +1000

    Fix style

--- src/Nest.php
+++ src/Nest.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Ozzie\Nest;
 
-use Closure;
-
 class Nest
 {
   /**
@@ -13,7 +11,7 @@ class Nest
    */
   public static array $prefixes = [];
 
-  public static function describe(string $description, Closure $callback): void
+  public static function describe(string $description, \Closure $callback): void
   {
     static::$prefixes[] = $description;
 
@@ -22,7 +20,7 @@ class Nest
     array_pop(static::$prefixes);
   }
 
-  public static function when(string $description, Closure $callback): void
+  public static function when(string $description, \Closure $callback): void
   {
     static::describe("when $description", $callback);
   }
@@ -32,7 +30,7 @@ class Nest
    */
   public static function it(
     string $description = null,
-    Closure $callback = null
+    \Closure $callback = null
   ) {
     $testName = implode(' - ', [...static::$prefixes, "it $description"]);
 
@@ -44,7 +42,7 @@ class Nest
    */
   public static function test(
     string $description = null,
-    Closure $callback = null
+    \Closure $callback = null
   ) {
     $testName = implode(' - ', [...static::$prefixes, $description]);
 
@@ -56,7 +54,7 @@ class Nest
    */
   public static function nativePestTest(
     string $description = null,
-    Closure $callback = null
+    \Closure $callback = null
   ) {
     return \test($description, $callback);
   }
